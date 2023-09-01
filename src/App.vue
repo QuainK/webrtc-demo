dd316101febbd4e2e658c3b7c11c258d86380f7e
Qian Kun <kqian@dawntech-info.com>
2023-09-01 16:58:24 +0800

    feat: 向FS注册SIP

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
 
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import * as JsSip from 'jssip'
 
 // 按钮状态
 const active = ref(false)
@@ -31,21 +32,52 @@ const onClickButton = () => {
  * 启动服务
  */
 const startService = () => {
-  active.value = true
   console.log('%c 启动服务', 'padding: 0 10em; background-color: green; color: #fff')
+  active.value = true
+  initSip()
 }
 /**
  * 停止服务
  */
 const stopService = () => {
-  active.value = false
   console.log('%c 停止服务', 'padding: 0 10em; background-color: grey; color: #fff')
+  active.value = false
+  killSip()
 }
 // 关闭或刷新页面前停止服务
 window.addEventListener('beforeunload', () => {
   console.log('beforeunload')
   stopService()
 })
+
+const socket = new JsSip.WebSocketInterface('ws://192.168.23.84:5066')
+const config = {
+  sockets: [socket],
+  uri: 'sip:1005@192.168.23.84',
+  password: '1234',
+}
+const ua = new JsSip.UA(config)
+ua.on('connecting', () => {
+  console.log('connecting')
+})
+ua.on('connected', () => {
+  console.log('connected')
+})
+ua.on('disconnected', () => {
+  console.log('disconnected')
+})
+
+const initSip = () => {
+  ua.start()
+
+  setTimeout(() => {
+    stopService()
+  }, 3000)
+}
+
+const killSip = () => {
+  ua.stop()
+}
 </script>
 
 <style scoped>
